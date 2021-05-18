36f0ebaac76090f887e60fe0f548a4890672aabc
giridhar7632 <giridhar.talla2002@gmail.com>
2021-05-18 14:01:20 +0530

    ex 1.6-1.14

--- part-01/02-unicafe/src/App.js
+++ part-01/02-unicafe/src/App.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
+const Statistic = ({ label, value }) => {
+  return (
+    <tr>
+      <td>{label}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, bad, neutral }) => {
+  const all = good + neutral + bad
+
+  if (all > 0) {
+    const average = (good - bad) / all
+    const positive = (good / all) * 100
+    return (
+      <table>
+        <tbody>
+          <Statistic label='good' value={good} />
+          <Statistic label='neutral' value={neutral} />
+          <Statistic label='bad' value={bad} />
+          <Statistic label='all' value={all} />
+          <Statistic label='average' value={average} />
+          <Statistic label='positive' value={`${positive}%`} />
+        </tbody>
+      </table>
+    )
+  } else {
+    return <p>No feedback given!!!</p>
+  }
+}
 
 const App = () => {
-  return <h1>"Hello world"</h1>
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  )
 }
 
 export default App
--- part-01/03-anecdote/src/App.js
+++ part-01/03-anecdote/src/App.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  return <h1>"Hello world"</h1>
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [most, setMost] = useState(0)
+  const [points, setPoints] = useState([0, 0, 0, 0, 0, 0])
+
+  const random = (max) => {
+    setSelected(Math.floor(Math.random() * max))
+  }
+
+  const vote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    console.log(copy)
+    setPoints(copy)
+    setMost(
+      copy.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1]
+    )
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the Day</h1>
+      <p>{anecdotes[selected]}</p>
+      <span>
+        votes: <b>{points[selected]}</b>
+      </span>
+      <br />
+      <button onClick={vote}>vote 👍</button>
+      <button onClick={() => random(anecdotes.length)}>next anecdote ⏭</button>
+      <hr />
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[most]}</p>
+      <span>
+        votes: <b>{points[most]}</b>
+      </span>
+    </div>
+  )
 }
 
 export default App
