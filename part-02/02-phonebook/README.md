bb424693592300f569391a574ce33747e08dcdfb
giridhar7632 <giridhar.talla2002@gmail.com>
2021-05-18 22:08:36 +0530

    ex 2.6-2.8

--- part-02/02-phonebook/README.md
+++ part-02/02-phonebook/README.md
@@ -1,5 +1,5 @@
-# Course Information
+# Phonebook
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## EX. 2.1 - 2.5
+## EX. 2.6 - 2.10
---
+++ part-02/02-phonebook/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState()
+  const [filter, setFilter] = useState('')
+
+  return (
+    <div>
+      <h1>Search for</h1>
+      <Filter filter={filter} setFilter={setFilter} />
+      <h2>Phonebook</h2>
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newNum={newNum}
+        setNewNum={setNewNum}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+    </div>
+  )
+}
+
+export default App
