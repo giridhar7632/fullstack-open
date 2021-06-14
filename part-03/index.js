679f699840c37972911bf688de5c1c5ed9559029
giridhar7632 <giridhar.talla2002@gmail.com>
2021-06-14 09:20:28 +0530

    ex 3.11

--- part-03/index.js
+++ part-03/index.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static('build'))
+
 morgan.token('post', (req, res) => {
 	if (req.method === 'POST') {
 		return JSON.stringify(req.body)
